2a1e661a9644918770ff4a7758a1d71d9f1e1341
iw1fnw <60930559+iw1fnw@users.noreply.github.com>
2024-04-19 21:18:07 +0200

    Delete release.sh

--- release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-read VERSION < VERSION
-ARCHIVE=tfla-01-$VERSION.tar.bz2
-git archive --format=tar --prefix=tfla-01-$VERSION/ master | bzip2 > $ARCHIVE
-echo "Archive '$ARCHIVE' ready!"
